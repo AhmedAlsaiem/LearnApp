7725b8d6253a42197b7bab94cc5809954f8ef8b6
AhmedAlsaiem <ahmedalsaiem2025@gamil.com>
2024-01-30 17:50:34 +0200

    refactor code of sign up view

--- lib/features/authantication/presentation/view/widget/sign_up_view_body.dart
+++ lib/features/authantication/presentation/view/widget/sign_up_view_body.dart
@@ -1,12 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:i_learn/core/utils/appRouters.dart';
-import 'package:i_learn/core/utils/constants.dart';
-import 'package:i_learn/features/authantication/presentation/view/widget/custom_botton.dart';
-import 'package:i_learn/features/authantication/presentation/view/widget/custom_text_form_field.dart';
 import 'package:i_learn/features/authantication/presentation/view/widget/logo_and_text_section.dart';
-import 'package:i_learn/features/authantication/presentation/view/widget/navigation_from_authatication_signin_signup.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:i_learn/features/splash/presentation/view/widgets/custom_signup_section.dart';
 
 class SignUpViewBody extends StatelessWidget {
   SignUpViewBody({
@@ -24,48 +18,7 @@ class SignUpViewBody extends StatelessWidget {
             LogoAndTextSection(
               label: 'Regisster to your acount',
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 15),
-              child: Column(
-                children: [
-                  CustomTextFormField(
-                    label: 'User name',
-                    icon: Icon(MdiIcons.accountOutline),
-                    onchange: (value) {},
-                  ),
-                  SizedBox(height: PaddingManger.defultPadding),
-                  CustomTextFormField(
-                    label: 'Email',
-                    icon: Icon(Icons.email_outlined, size: 30),
-                    onchange: (value) {},
-                  ),
-                  SizedBox(height: PaddingManger.defultPadding),
-                  CustomTextFormField(
-                    label: 'Password',
-                    icon: Icon(Icons.lock_outlined, size: 30),
-                    onchange: (value) {},
-                  ),
-                  SizedBox(height: PaddingManger.defultPadding * 1.5),
-                  CustomButton(
-                    label: 'Sign Up',
-                    onPressed: () {
-                      formKey.currentState!.validate();
-                    },
-                  ),
-                  SizedBox(height: PaddingManger.defultPadding),
-                  NavigationFromAuthaticitonSignInSignUp(
-                    label1: 'You have an acount ',
-                    label2: 'Sign In',
-                    ontap: () {
-                      GoRouter.of(context).push(AppRouter.kLoginView);
-                    },
-                  ),
-                  SizedBox(
-                    height: PaddingManger.defultPadding * 2,
-                  ),
-                ],
-              ),
-            )
+            CustomSignUpWidget(formKey: formKey)
           ],
         ),
       ),
---
+++ lib/features/splash/presentation/view/widgets/custom_signup_section.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:i_learn/core/utils/appRouters.dart';
+import 'package:i_learn/core/utils/constants.dart';
+import 'package:i_learn/features/authantication/presentation/view/widget/custom_botton.dart';
+import 'package:i_learn/features/authantication/presentation/view/widget/custom_text_form_field.dart';
+import 'package:i_learn/features/authantication/presentation/view/widget/navigation_from_authatication_signin_signup.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
+class CustomSignUpWidget extends StatelessWidget {
+  const CustomSignUpWidget({
+    super.key,
+    required this.formKey,
+  });
+
+  final GlobalKey<FormState> formKey;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      child: Column(
+        children: [
+          CustomTextFormField(
+            label: 'User name',
+            icon: Icon(MdiIcons.accountOutline),
+            onchange: (value) {},
+          ),
+          SizedBox(height: PaddingManger.defultPadding),
+          CustomTextFormField(
+            label: 'Email',
+            icon: Icon(Icons.email_outlined, size: 30),
+            onchange: (value) {},
+          ),
+          SizedBox(height: PaddingManger.defultPadding),
+          CustomTextFormField(
+            label: 'Password',
+            icon: Icon(Icons.lock_outlined, size: 30),
+            onchange: (value) {},
+          ),
+          SizedBox(height: PaddingManger.defultPadding * 1.5),
+          CustomButton(
+            label: 'Sign Up',
+            onPressed: () {
+              formKey.currentState!.validate();
+            },
+          ),
+          SizedBox(height: PaddingManger.defultPadding),
+          NavigationFromAuthaticitonSignInSignUp(
+            label1: 'You have an acount ',
+            label2: 'Sign In',
+            ontap: () {
+              GoRouter.of(context).push(AppRouter.kLoginView);
+            },
+          ),
+          SizedBox(
+            height: PaddingManger.defultPadding * 2,
+          ),
+        ],
+      ),
+    );
+  }
+}
